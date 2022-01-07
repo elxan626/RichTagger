b2655c658205ea50fb69280e7972e9acebcec218
roxyconfig <92932568+roxyconfig@users.noreply.github.com>
2022-01-08 00:36:52 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -235,7 +235,7 @@ async def mentionall(event):
     async for usr in client.iter_participants(event.chat_id):
       usrnum += 1
       usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id}) "
-      if event.chat_id not in emoji_calisan:
+      if event.chat_id not in anlik_calisan:
         await event.respond("** Etiket işlemi başarıyla durduruldu❌**")
         return
       if usrnum == 5:
