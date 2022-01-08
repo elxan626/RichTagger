25fe2a4bba330dd1c3b3acf7fb00e01af7e457bd
roxyconfig <92932568+roxyconfig@users.noreply.github.com>
2022-01-08 08:23:29 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -314,7 +314,7 @@ async def mentionall(event):
   
   @client.on(events.NewMessage(pattern='^(?i)/cancel'))
   async def cancel(event):
-  global emoji_calisan
+  global anlik_calisan
   anlik_calisan.remove(event.chat_id)
 
 
